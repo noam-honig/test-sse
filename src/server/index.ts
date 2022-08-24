7f65787510d97cd1a8ed12ec8abeb2688a6ad228
Noam Honig <noam.honig@gmail.com>
2022-08-24 17:49:37 +0300

    push

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,32 @@
 import express from 'express';
 import { api } from './api';
 import { sse } from './sse';
+import puppeteer from 'puppeteer';
 
 const app = express();
 app.use(api);
 app.use(sse);
+app.get('/api/test', async (req, res) => {
+    for (let index = 0; index < 300; index++) {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        page.goto('https://mighty-tor-87921.herokuapp.com/', { waitUntil: 'networkidle2' });
+        console.log(index);
 
+    }
+    res.send("started 300");
+});
 import path from 'path';
 app.use(express.static(path.join(__dirname, '../')));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../', 'index.html'));
 });
 
+(async () => {
+
+
+})();
+
+
 app.listen(process.env["PORT"] || 3002, () => console.log("Server started"));
+
